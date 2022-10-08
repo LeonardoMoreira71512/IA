7349451e552a5716b1b2030bcebc2341dc64ab73
LeonardoMoreira71512 <leonardo2002moreira@gmail.com>
2022-10-08 19:03:09 +0100

    Implemented the structure of the code

---
+++ src/Board.java
@@ -0,0 +1,32 @@
+class Board implements Ilayout, Cloneable {
+        private static final int dim=3;
+        private int board[][];
+
+        public Board(){
+            board = new int[dim][dim];
+        }
+
+        public Board(String str) throws IllegalStateException {
+            if (str.length() != dim*dim)
+                throw new IllegalStateException("Invalid arg in Board constructor");
+            board = new int[dim][dim];
+            int si=0;
+            for(int i=0; i<dim; i++)
+                for(int j=0; j<dim; j++)
+                    board[i][j] = Character.getNumericValue(str.charAt(si++));
+        }
+
+        public String toString(){
+            //TO BE COMPLETED
+        }
+
+        public boolean equals(Object o){
+
+        }
+
+        public int hashCode(){
+            return super.hashCode();
+        }
+
+
+}
